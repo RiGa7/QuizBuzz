3e35245319163bffd1512bce4dbf9c2c431b1d1a
Rishika <rishikamanoj61976@gmail.com>
2026-01-11 13:49:08 +0530

    Update correct wrong UI

--- app/src/main/java/com/example/quizzbuzz/QuizActivity.java
+++ app/src/main/java/com/example/quizzbuzz/QuizActivity.java
@@ -1,8 +1,15 @@
 package com.example.quizzbuzz;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -23,7 +30,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class QuizActivity extends AppCompatActivity {
-    private  int categoryId;
+    private int categoryId;
 
     private ProgressBar progressBar;
     private TextView currentQuestionNo;
@@ -33,7 +40,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button nextBtn;
     List<Question> questions;
     private int count;
-    int score =0;
+    int score = 0;
 
 
     @Override
@@ -47,6 +54,10 @@ public class QuizActivity extends AppCompatActivity {
             return insets;
         });
 
+        LinearLayout mainLayout = findViewById(R.id.main);
+        mainLayout.setVisibility(View.GONE);
+        TextView loadingText = findViewById(R.id.loadingText);
+        loadingText.setVisibility(View.VISIBLE);
 
         String category = getIntent().getStringExtra("category");
         String difficulty = getIntent().getStringExtra("difficulty");
@@ -56,7 +67,7 @@ public class QuizActivity extends AppCompatActivity {
         question = findViewById(R.id.question);
         currentQuestionNo = findViewById(R.id.currentQuestion);
 
-        nextBtn = findViewById(R.id.playAgainBtn);
+        nextBtn = findViewById(R.id.nextBtn);
 
         nextBtn.setEnabled(false);
         nextBtn.setText("Next");
@@ -66,31 +77,31 @@ public class QuizActivity extends AppCompatActivity {
                 findViewById(R.id.option1), findViewById(R.id.option2),
                 findViewById(R.id.option3), findViewById(R.id.option4)
         };
-
         optionsGrp.setOnCheckedChangeListener((group, checkedId) -> {
-            nextBtn.setEnabled(true);
+            if (checkedId != -1) {
+                nextBtn.setEnabled(true);
+            }
         });
 
-
         if (category != null) {
-            switch (category){
+            switch (category) {
                 case "General Knowledge":
                     categoryId = 9;
                     break;
                 case "Science":
-                    categoryId= 17;
+                    categoryId = 17;
                     break;
                 case "Technology":
-                    categoryId= 18;
+                    categoryId = 18;
                     break;
                 case "Geography":
-                    categoryId= 22;
+                    categoryId = 22;
                     break;
                 case "Sports":
-                    categoryId=21;
+                    categoryId = 21;
                     break;
                 default:
-                    categoryId= 26;
+                    categoryId = 26;
                     break;
             }
         }
@@ -111,7 +122,6 @@ public class QuizActivity extends AppCompatActivity {
         call.enqueue(new Callback<Result>() {
             @Override
             public void onResponse(Call<Result> call, Response<Result> response) {
-
                 if (response.isSuccessful() && response.body() != null) {
                     questions = response.body().getResults();
                     if (questions == null || questions.isEmpty()) {
@@ -119,16 +129,20 @@ public class QuizActivity extends AppCompatActivity {
                         nextBtn.setEnabled(false);
                         return;
                     }
+                    loadingText.setVisibility(View.GONE);
+                    mainLayout.setVisibility(View.VISIBLE);
 
-                    count =0;
+                    count = 0;
                     displayQuestion();
                 }
             }
 
             @Override
             public void onFailure(Call<Result> call, Throwable t) {
-                question.setText("QUIZ", TextView.BufferType.valueOf(t.getMessage()));
+                Log.e("onFailure: ", t.getMessage());
             }
+
+
         });
 
         nextBtn.setOnClickListener(v -> {
@@ -140,51 +154,75 @@ public class QuizActivity extends AppCompatActivity {
             }
 
             RadioButton selectedOption = findViewById(selectedId);
-            String userAnswer = selectedOption.getText().toString();
 
-            Question currentQuestion = questions.get(count);
-            String correctAnswer = currentQuestion.getCorrect_answer();
-
-            if (userAnswer.equals(correctAnswer)) {
-                score++;
-            }
-
+            answer(selectedOption);
             count++;
-
-            if (count < questions.size()) {
-                displayQuestion();
-                if (count == questions.size() - 1) {
-                    nextBtn.setText("Submit");
+            nextBtn.postDelayed(()->{
+                if (count < questions.size()) {
+                    displayQuestion();
+                    if (count == questions.size() - 1) {
+                        nextBtn.setText("Submit");
+                    }
+                } else {
+                    Intent i = new Intent(QuizActivity.this, ResultActivity.class);
+                    i.putExtra("score", String.valueOf(score));
+                    startActivity(i);
+                    finish();
                 }
-            } else {
-                Intent i = new Intent(QuizActivity.this, ResultActivity.class);
-                i.putExtra("score",String.valueOf(score));
-                startActivity(i);
-                finish();
-            }
-    });
+            },2000);
+        });
     }
-
     private void displayQuestion() {
+        resetOptions();
         optionsGrp.clearCheck();
-        nextBtn.setEnabled(false);
 
         Question q = questions.get(count);
 
-        question.setText(q.getQuestion());
+        question.setText(Html.fromHtml(q.getQuestion(),Html.FROM_HTML_MODE_LEGACY));
         currentQuestionNo.setText(String.valueOf(count + 1));
-        int currentProgress = progressBar.getProgress();
-        progressBar.setProgress(currentProgress+1);
+        progressBar.setMax(questions.size());
+        progressBar.setProgress(count + 1);
 
         List<String> answers = q.getAllAnswers();
         Collections.shuffle(answers);
 
         for (int i = 0; i < 4; i++) {
-            options[i].setText(answers.get(i));
+            options[i].setText(Html.fromHtml(answers.get(i)));
+        }
+    }
+    private void resetOptions() {
+        for (RadioButton option : options) {
+            option.setBackgroundResource(R.drawable.option_bg);
+            option.setEnabled(true);
+        }
+    }
+    private void answer(RadioButton selectedOption) {
+        String userAnswer = Html.fromHtml(selectedOption.getText().toString(), Html.FROM_HTML_MODE_LEGACY).toString().trim();
+
+        Question currentQuestion = questions.get(count);
+        String correctAnswer = Html.fromHtml(currentQuestion.getCorrect_answer(), Html.FROM_HTML_MODE_LEGACY).toString().trim();
+        ;
+
+        for (RadioButton option : options) {
+            option.setEnabled(false);
+        }
+
+        if (userAnswer.equals(correctAnswer)) {
+            selectedOption.setBackgroundResource(R.drawable.correct_option_bg);
+            score++;
+            return;
+        }
+
+        selectedOption.setBackgroundResource(R.drawable.wrong_option_bg);
+
+        for (RadioButton option : options) {
+            if (option.getText().toString().equals(correctAnswer)) {
+                option.setBackgroundResource(R.drawable.correct_option_bg);
+                break;
+            }
         }
     }
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
-
 }
